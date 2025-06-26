71f4cd85e427a95e91a1b7f77dbd20d4cfa0defe
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-06-26 19:49:40 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import NavBar from './Component/NavBar'
-import UseEffect from './Component/UseEffect'
+import DataFetch from './Component/DataFetch'
 function App() {
 
   return (
     <div className='w-full overflow-x-hidden'>
       <NavBar title="WebPage" />
-
-      <UseEffect/>
+      <DataFetch />
     </div>
   )
 }
---
+++ src/Component/UseFetch.jsx
@@ -0,0 +1,35 @@
+import React ,{useState, useEffect} from 'react'
+
+const UseFetch = (url) => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then((res) => {
+                    if (!res.ok) {
+                        throw Error(" todos are not loading, try later");
+
+                    } else {
+
+                        return res.json();
+                    }
+                })
+                .then((data) => {
+                    setData(data);
+                    setLoading(false);
+                    setError(false)
+                })
+                .catch((error) => {
+                    setLoading(false)
+                    setError(error.message)
+                })
+        }, 3000);
+    }, [])
+    return {data, loading, error}
+}
+
+export default UseFetch
