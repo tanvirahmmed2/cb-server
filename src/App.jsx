e0fbd1a0eb190032b2a977dbfff219f1c07fb7cf
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-06-30 21:48:16 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+
+import { v4 as uuidv4 } from 'uuid';
+
+
 import NavBar from './Component/NavBar'
 import ToastMasg from './Component/ToastMasg'
 import FetchData from './Component/Test/FetchData'
-function App() {
+import Country from './Component/Country/Country'
 
+
+const url= "https://restcountries.com/v3.1/all";
+function App() {
+  const [loading, setLoading]= useState(true);
+  const [countries, setCountries]= useState([]);
+  const [error, setError]=useState(false);
+ const fetchData=()=>{
+  try {
+    const response= fetch(url).json();
+    const countries= response.data()
+    setCountries(countries)
+    setLoading(false)
+    setError(false)
+  } catch (error) {
+    setError(true)
+    setLoading(false)
+  }
+ 
+  
+  
+ }
+  useEffect(()=>{
+    setTimeout(() => {
+      fetchData()
+    }, 3000);
+  },[])
+
+const loadingmasg= <h1>Loading</h1>
+const errmasg= <h1>Fetching problem</h1>
   return (
     <div className='w-full overflow-x-hidden'>
-      <NavBar title="WebPage" />
-     <FetchData/>
+      
+      <h1>Hello</h1>
+      {loading && loadingmasg}
+      {
+        countries.map((country, id= uuidv4)=>{
+          return <div key={id}>
+            <h1>{country.name}</h1>
+
+          </div>
+        })
+      }
+      {error && errmasg}
     </div>
   )
 }
