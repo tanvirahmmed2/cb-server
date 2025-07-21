dad30a5233daa52cfdd4f4429b269852acfce644
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-07-21 20:30:01 +0600

    update

--- src/Component/Redux/Index.jsx
+++ src/Component/Redux/Index.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div className='text-5xl text-red-600 font-bold'>
-      hello reduxxxxxx
+    <div className='w-full h-auto flex-col flex'>
+      
+      <div className='w-[400px] h-[300px] bg-amber-400'></div>
+      
     </div>
   )
 }
