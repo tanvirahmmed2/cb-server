6a9bc916beb35f8d6fb2e54adcd94ecd196c4123
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-25 21:47:32 +0600

    prompt

--- src/controller/chat.controller.js
+++ src/controller/chat.controller.js
@@ -13,11 +13,8 @@ const sendPromt = async (req, res) => {
             })
         }
         const mainPrompt = `
-      Create a ${language} social media post about: "${prompt}".
-      Include:
-      1. A catchy title
-      2. A short creative post body (~100 words)
-      3. 5 relevant hashtags
+        "${prompt}" in ${language} .
+      
     `;
 
         const completion = await openai.chat.completions.create({
